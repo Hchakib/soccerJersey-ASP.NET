14a440993d70ee085680216f3dd5c388cd83ec40
Chakib <chakibhocine724@gmail.com>
2025-05-28 21:47:35 -0400

    Initial commit of my ASP.NET Soccer Jersey project

---
+++ Football/Models/AnnuaireContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Football.Models
+{
+        public class AnnuaireContext : DbContext
+        {
+            public DbSet<Maillot> Maillots { get; set; }
+
+            public AnnuaireContext(DbContextOptions options) : base(options)
+            {
+
+            }
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                //base.OnConfiguring(optionsBuilder);
+            }
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<Maillot>().Property(m => m.Description).IsRequired();
+                modelBuilder.Entity<Maillot>().Property(m => m.Couleur).IsRequired();
+                modelBuilder.Entity<Maillot>().Property(m => m.Picture).IsRequired();
+                modelBuilder.Entity<Maillot>().Property(m => m.Taille).IsRequired();
+                modelBuilder.Entity<Maillot>().Property(m => m.Prix).IsRequired();
+                modelBuilder.Entity<Maillot>().Property(m => m.Pays).IsRequired();
+            }
+        }
+}
